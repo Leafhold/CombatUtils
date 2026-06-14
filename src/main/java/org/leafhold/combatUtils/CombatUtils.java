d3619e5599511fd474a7c9e79faf33795384711d
LookAtFr3sn0 <contact@lookatfr3sn0.com>
2026-06-14 16:22:20 +0200

    Initial commit

---
+++ src/main/java/org/leafhold/combatUtils/CombatUtils.java
@@ -0,0 +1,16 @@
+package org.leafhold.combatUtils;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CombatUtils extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
